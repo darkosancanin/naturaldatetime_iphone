95043d82223b5d1800a7eeef489a69dc5a5f4088
Darko Sancanin <darkosancanin@mbp.local>
2015-07-16 22:17:33 +1000

    Updated trial layout. Working with minor issues with footer.

--- naturaldateandtime/QuestionViewController.swift
+++ naturaldateandtime/QuestionViewController.swift
@@ -4,6 +4,7 @@ class QuestionViewController: UIViewController, UITextViewDelegate {
     
     @IBOutlet weak var contentView: UIView!
     @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var outerNotesView: UIScrollView!
     
     override func viewDidLoad() {
         var leftConstraint: NSLayoutConstraint = NSLayoutConstraint(item: self.contentView, attribute: NSLayoutAttribute.Leading, relatedBy: NSLayoutRelation.Equal, toItem: self.view, attribute: NSLayoutAttribute.Left, multiplier: 1.0, constant: 0)
@@ -11,6 +12,9 @@ class QuestionViewController: UIViewController, UITextViewDelegate {
         
         var rightConstraint: NSLayoutConstraint = NSLayoutConstraint(item: self.contentView, attribute: NSLayoutAttribute.Trailing, relatedBy: NSLayoutRelation.Equal, toItem: self.view, attribute: NSLayoutAttribute.Right, multiplier: 1.0, constant: 0)
         self.view.addConstraint(rightConstraint)
+        
+        var heightConstraint: NSLayoutConstraint = NSLayoutConstraint(item: self.contentView, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.GreaterThanOrEqual, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1.0, constant: self.view.frame.size.height - self.navigationController!.navigationBar.frame.size.height - 20)
+        self.view.addConstraint(heightConstraint)
         
         self.automaticallyAdjustsScrollViewInsets = false
     }
